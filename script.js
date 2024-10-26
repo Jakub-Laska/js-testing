f2350d323744bb44765f895a95c2b936073c3580
Klaska <laska.rayl@gmail.com>
2024-10-26 02:36:36 +0200

    shopping list

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-const container = document.querySelector('#container');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const list = document.querySelector('ul');
 
-const para = document.createElement('p');
-para.textContent = 'Hey I\'m red!';
-para.style.color = 'red';
 
-container.appendChild(para);
+btn.addEventListener('click', () => {
+    let inputValue = input.value;
+    input.value = '';
+    const listItem = document.createElement('li');
+    const span = document.createElement('span');
+    const deleteBtn = document.createElement('button');
 
-const hThree = document.createElement('h3');
-hThree.textContent = 'Hey I\'m blue!';
-hThree.style.color = 'blue';
+    listItem.appendChild(span);
+    listItem.appendChild(deleteBtn);
 
-container.appendChild(hThree);
+    span.textContent = inputValue;
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
+        
+    list.appendChild(listItem);
 
-const div = document.createElement('div');
-div.style.border = '1px solid black';
-div.style.backgroundColor = 'pink';
+    input.focus();
+});
 
-container.appendChild(div);
 
-const hOne = document.createElement('h1');
-hOne.textContent = 'I\m in a div';
-
-div.appendChild(hOne);
-
-const paraTwo = document.createElement('p');
-paraTwo.textContent = 'ME TOO!';
-
-div.appendChild(paraTwo);
